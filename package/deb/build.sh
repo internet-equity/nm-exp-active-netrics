e519827129a70d1b105ba89ea99bf204e9e7d80c
ggmartins <ggmartins@gmail.com>
2021-02-25 20:53:14 -0600

    feat: build.sh for .deb packaging

---
+++ package/deb/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#PROC=$(uname -a)
+PROC=arm64
+mv *.deb backup/
+version_pyc=$(cat ../../src/netrics.py | grep "__version__" | grep -Po '(\d)+.(\d)+.(\d)+')
+version_deb=$(cat nm-exp-active-netrics/DEBIAN/control| grep "Version:" | grep -Po '(\d)+.(\d)+.(\d)+')
+echo "python version:" ${version_pyc}
+echo "debian version:" ${version_deb}
+
+cp -R /usr/local/src/nm-exp-active-netrics/src/ nm-exp-active-netrics/usr/local/src/nm-exp-active-netrics/
+cp -R /usr/local/src/nm-exp-active-netrics/venv/ nm-exp-active-netrics/usr/local/src/nm-exp-active-netrics/
+
+mkdir -p nm-exp-active-netrics/etc/nm-exp-active-netrics/
+mkdir -p nm-exp-active-netrics/etc/init.d/
+mkdir -p nm-exp-active-netrics/usr/local/src/nm-exp-active-netrics/databases
+mkdir -p nm-exp-active-netrics/usr/local/src/nm-exp-active-netrics/bin/
+mkdir -p nm-exp-active-netrics/usr/local/bin
+mkdir -p nm-exp-active-netrics/usr/bin
+cp ../../conf/nm-exp-active-netrics.toml nm-exp-active-netrics/etc/nm-exp-active-netrics/
+cp /etc/init.d/nm-exp-active-netrics nm-exp-active-netrics/etc/init.d/
+cp /usr/local/src/nm-exp-active-netrics/bin/netrics nm-exp-active-netrics/usr/local/src/nm-exp-active-netrics/bin/
+cp /usr/bin/nm-exp-active-netrics nm-exp-active-netrics/usr/bin/
+fakeroot dpkg-deb --build nm-exp-active-netrics
+mv nm-exp-active-netrics.deb nm-exp-active-netrics-v${version_pyc}-${PROC}.deb
+
--- src/netrics.py
+++ src/netrics.py
@@ -13,7 +13,7 @@ log = logging.getLogger(__name__)
 
 #TODO: move this to setup.py
 __name__ = "nm-exp-active-netrics"
-__version__ = "0.0.1"
+__version__ = "0.1.10"
 __author__ = "Kyle-MacMillan, James Saxon, Guilherme Martins"
 
 def build_parser():
