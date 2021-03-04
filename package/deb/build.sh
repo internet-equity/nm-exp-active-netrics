0007ceead8ea49fc4d3c88d63687506a71a87e38
ggmartins <ggmartins@gmail.com>
2021-03-03 19:23:57 -0600

    fix: including iperf3 libs

--- package/deb/build.sh
+++ package/deb/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#PROC=$(uname -a)
+PROC_U=$(uname -p)
 PROC=arm64
 mv *.deb backup/
 version_pyc=$(cat ../../src/netrics.py | grep "__version__" | grep -Po '(\d)+.(\d)+.(\d)+')
@@ -17,11 +17,18 @@ mkdir -p nm-exp-active-netrics/usr/local/src/nm-exp-active-netrics/databases
 mkdir -p nm-exp-active-netrics/usr/local/src/nm-exp-active-netrics/bin/
 mkdir -p nm-exp-active-netrics/usr/local/bin
 mkdir -p nm-exp-active-netrics/usr/bin
+
+#netrics
 cp ../../conf/nm-exp-active-netrics.toml nm-exp-active-netrics/etc/nm-exp-active-netrics/
 cp /etc/init.d/nm-exp-active-netrics nm-exp-active-netrics/etc/init.d/
 cp /usr/local/src/nm-exp-active-netrics/bin/netrics nm-exp-active-netrics/usr/local/src/nm-exp-active-netrics/bin/
 cp /usr/local/src/nm-exp-active-netrics/bin/netrics nm-exp-active-netrics/usr/local/bin/
 cp /usr/bin/nm-exp-active-netrics nm-exp-active-netrics/usr/bin/
+
+#iperf3
+cp /usr/local/bin/iperf3 nm-exp-active-netrics/usr/local/bin/
+cp -r ../../commands/$PROC_U/lib/ nm-exp-active-netrics/usr/
+
 fakeroot dpkg-deb --build nm-exp-active-netrics
 mv nm-exp-active-netrics.deb nm-exp-active-netrics-v${version_pyc}-${PROC}.deb
 
