f837271a39bf35879f48a6a8bc5a3a591a5d294c
ggmartins <ggmartins@gmail.com>
2021-05-10 16:43:51 -0500

    fix: makecron build + reboot

--- package/deb/build.sh
+++ package/deb/build.sh
@@ -48,6 +48,7 @@ cp /usr/bin/nm-exp-active-netrics nm-exp-active-netrics/usr/bin/
 cp ../../commands/$PROC_U/bin/* nm-exp-active-netrics/usr/local/src/nm-exp-active-netrics/bin/
 cp ../../commands/$PROC_U/lib/* nm-exp-active-netrics/usr/local/src/nm-exp-active-netrics/lib/
 cp /usr/local/src/nm-exp-active-netrics/bin/iperf3.sh nm-exp-active-netrics/usr/local/src/nm-exp-active-netrics/bin/
+cp /usr/local/src/nm-exp-active-netrics/bin/makecron.py nm-exp-active-netrics/usr/local/src/nm-exp-active-netrics/bin/
 
 fakeroot dpkg-deb --build nm-exp-active-netrics
 mv nm-exp-active-netrics.deb nm-exp-active-netrics-v${version_pyc}-${PROC}.deb
