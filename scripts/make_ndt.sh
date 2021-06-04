8c3b9d695f73030a2dace122004e4e90d78048ac
ggmartins <ggmartins@gmail.com>
2021-06-04 14:23:43 -0500

    feat: make_ndt building from git

--- scripts/make_ndt.sh
+++ scripts/make_ndt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $EUID -eq 0 ]]; then
-   echo "ERROR: This script must be run as non-root" 
+	echo "ERROR: This script must run as NON-root, (from an user with sudo priv)" 
    exit 1
 fi
 
@@ -12,13 +12,23 @@ NMAPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
 cd $NMAPATH
 echo "INFO: working dir ("$(pwd)")"
 
-#mkdir -p commands/src/ndt/$PROC/
-#cd commands/src/ndt/$PROC/
+echo $PWD
+sudo mkdir -p commands/src/ndt/$PROC/
+sudo chown $USER:$USER commands/src/ndt/$PROC/
+cd commands/src/ndt/$PROC/
 
 if cmd=$(command -v go); then echo $cmd; else echo "ERROR: golang not in the path"; exit 1; fi
 go version
-USER1=$USER
-sudo chown $USER1:$USER1 commands/$PROC -R
 export GOPATH=$PWD/commands/$PROC
 export GO111MODULE=on
-go get -v github.com/m-lab/ndt7-client-go/cmd/ndt7-client
+echo $PWD
+#go get -v github.com/m-lab/ndt7-client-go/cmd/ndt7-client
+
+mkdir gocache
+export GOCACHE=$PWD/gocache
+export GOPATH=$PWD
+
+git clone https://github.com/m-lab/ndt7-client-go.git
+cd ndt7-client-go/cmd/ndt7-client
+go build -o ndt7-client main.go
+cp ndt7-client $NMAPATH/commands/$PROC/bin/
