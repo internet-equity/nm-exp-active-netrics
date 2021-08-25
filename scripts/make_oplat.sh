e286f38d9ccb5b182b1e2e5e75044008dabb79aa
Kyle MacMillan <macmillan@uchicago.edu>
2021-08-25 15:27:28 -0500

    Updated make_oplat script

--- scripts/make_oplat.sh
+++ scripts/make_oplat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [[ $EUID -eq 0]]; then
-   echo "ERROR: this script must run as NON-root, (from a user with sudo priv)"
+if [[ $EUID -eq 0 ]]; then
+      echo "ERROR: this script must run as NON-root, (from a user with sudo priv)"
    exit 1
 fi
 
@@ -17,8 +17,13 @@ sudo mkdir -p commands/src/oplat/$PROC/
 sudo chown $USER:$USER commands/src/oplat/$PROC/
 cd commands/src/oplat/$PROC
 
+wget https://golang.org/dl/go1.17.linux-arm64.tar.gz
+tar -xzf go1.17.linux-arm64.tar.gz
+export PATH=$PATH:~/go/bin
+
 if cmd=$(command -v go); then echo $cmd; else echo "ERROR: golang not in the path"; exit 1; fi
 
+go get -u github.com/go-ping/ping
 git clone https://github.com/kyle-macmillan/OpLat.git
 cd OpLat
 go build oplat.go
