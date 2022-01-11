2ed3cdceaa90219c363fa12171d8c845795752ba
Marc Richardson <marc.t.richardson@gmail.com>
2022-01-11 21:25:25 +0000

    fix: update make_speedtest.sh

--- scripts/make_speedtest.sh
+++ scripts/make_speedtest.sh
@@ -14,10 +14,10 @@ if [ -f "/usr/bin/speedtest" ];then
   cp -f /usr/bin/speedtest ./commands/${PROC}/bin/
 else
   sudo apt-get install -y gnupg1 apt-transport-https dirmngr
-  export INSTALL_KEY=379CE192D401AB61
-  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $INSTALL_KEY
-  echo "deb https://ookla.bintray.com/debian generic main" | sudo tee  /etc/apt/sources.list.d/speedtest.list
-  sudo apt-get update
+
+  # https://www.speedtest.net/apps/cli
+  curl -s https://install.speedtest.net/app/cli/install.deb.sh | sudo bash
+
   # Other non-official binaries will conflict with Speedtest CLI
   # Example how to remove using apt-get
   # sudo apt-get remove speedtest-cli
