0945156b52ca4aa5ec7b60d5faa2eb7f936fd328
ggmartins <ggmartins@gmail.com>
2022-12-21 22:55:56 +0000

    feat: fix for Dockerfile.template

--- package/docker/anon/netrics/cron.sh
+++ package/docker/anon/netrics/cron.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "NETRICS INFO: Netrics restart..."
+dpkg -i ./nm-exp-active-netrics-v1.0.0-arm64.deb
 /etc/init.d/nm-exp-active-netrics restart
 chown netrics:netrics /var/nm/nm-exp-active-netrics/ -R
 cron -f
