8b9a5dea1e388e8f0a16563cb4bac04043b5df0a
ggmartins <ggmartins@gmail.com>
2023-02-13 20:20:29 +0000

    fix: missing utils.py

---
+++ src/netrics/utils.py
@@ -0,0 +1,7 @@
+from subprocess import Popen, PIPE
+
+def popen_exec(cmd):
+    pipe = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+    out = pipe.stdout.read().decode('utf-8')
+    err = pipe.stderr.read().decode('utf-8')
+    return out, err
