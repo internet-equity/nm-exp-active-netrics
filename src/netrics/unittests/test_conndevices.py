29bc853d9213cf138d6cbc1e22b9c67d2ea05655
Fathima Maha <fathima.nitt@gmail.com>
2023-03-02 06:33:20 +0000

    unittests: conn_devices-to fix dinydb

---
+++ src/netrics/unittests/test_conndevices.py
@@ -0,0 +1,97 @@
+
+from tinydb import TinyDB
+from pathlib import Path
+import toml
+import unittest
+from pathlib import Path
+import sys
+import warnings
+import json
+sys.path.insert(0, '/home/ubuntu/nm-exp-active-netrics/src')
+from netrics.builtin import netrics_test_latunderload as netrics
+from mock_measurements import Measurements
+
+devdb = TinyDB(Path(str(Path(__file__).resolve().parent)+'/samples/devdb.json'))
+
+class TestConnDevices(unittest.TestCase):
+    """Class for methods of connected devices unit tests"""
+    
+    def setUp(self):
+        self.measurement = Measurements()
+        self.key = "connected_devices"
+
+    def test_latunderload_success(self):
+        """
+        Test flow correctness if inputs given are correct
+        """
+    
+        #read config
+        conf = self.read_config('nm-exp-active-netrics.toml')
+
+        #run test
+        results = {}
+        results[self.key] = {}
+
+        try:
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+                output = netrics.test_latunderload(self.key, self.measurement, devdb, conf, results, False)
+        except Exception as e:
+            print("ERROR IN TEST SUITE: ", e)
+            return
+
+        self.assertIsNotNone(results)
+        self.assertIsNotNone(output)
+        print(results)
+        print(output)
+
+
+        # #assert output format
+        # json_sample = str(Path(__file__).resolve().parent) + '/samples/latunder_output.json'
+        # try:
+        #     with open(json_sample, "rb") as f:
+        #         json_format = json.loads(f.read().decode('utf-8'))
+        # except Exception as e:
+        #     print("ERROR IN TEST SUITE: ", e)
+        #     return   
+        
+        # self.assertEqual(type(output),dict)
+        # self.assert_format(json_format, output)
+
+        # #assert result format
+        # json_sample = str(Path(__file__).resolve().parent) + '/samples/latunder_results.json'
+        # try:
+        #     with open(json_sample, "rb") as f:
+        #         json_format = json.loads(f.read().decode('utf-8'))
+        # except Exception as e:
+        #     print("ERROR IN TEST SUITE: ", e)
+        #     return   
+        
+        # self.assertEqual(type(results),dict)
+        # self.assert_format(json_format, results)
+
+        return
+
+    def assert_format(self, format_dict, check_dict):
+        """
+        checks if keys are matching ina format dict to 
+        a refernce dict
+        """
+        for key in format_dict:
+            self.assertIn(key,check_dict)
+            if type(key) is dict:
+                self.assert_format(format_dict[key],check_dict[key])
+        return
+
+    def read_config(self, filename):
+        config_file = str(Path(__file__).resolve().parent) + '/test_configs/'+filename
+        try:
+            with open(config_file, "rb") as f:
+                conf = toml.loads(f.read().decode('utf-8'))
+        except Exception as e:
+            print(e)
+        return conf
+
+    
+if __name__ == "__main__":
+    unittest.main()
