482c869002a728268c0a6928209ef8f9f32dd82d
Fathima Maha <fathima.nitt@gmail.com>
2023-03-22 18:33:40 +0000

    plugins: init

---
+++ src/netrics/plugins/netrics_test_goresp.py
@@ -0,0 +1,16 @@
+from .. utils import popen_exec
+import logging
+import time
+from subprocess import Popen, PIPE
+from tinydb import where
+from tinydb.operations import increment
+from tinydb.operations import set as tdb_set
+
+log = logging.getLogger(__name__)
+
+def test_goresp(key, measurement, dev_db, conf, results, quiet):
+
+
+    res = {}
+
+    return res
