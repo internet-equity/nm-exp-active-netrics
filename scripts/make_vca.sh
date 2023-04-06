137a45a0c3ef4b4a1fc33efe9398d747dc847649
tarunmangla <mangla.tarun92@gmail.com>
2023-04-06 17:48:13 +0000

    additional packages

--- scripts/make_vca.sh
+++ scripts/make_vca.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+if [[ $EUID -ne 0 ]]; then
+   echo "ERROR: This script must be run as root" 
+   exit 1
+fi
+
+
 ## install software
-sudo apt install snapd
-sudo snap install chromium
+apt -y install snapd python3-dev python3-opencv xvfb python3-tk python3-dev scrot xclip xdg-utils
+snap install chromium
 
 ## create xauthority file needed for the virtual display
+## FIX
 touch ~/.Xauthority
-
-## download the vca code
-git submodule update --init --recursive
-git submodule update --remote
 
 ## copy the config file
 VCA_PATH="src/netrics/plugin/netrics_vca_test"
@@ -20,8 +23,15 @@ cp "$VCA_AUTO_PATH/configs/config_client.toml_bkp" "$VCA_AUTO_PATH/config.toml"
 ### activate the virtual environment
 if [ ! "$VIRTUAL_ENV" == "/usr/local/src/nm-exp-active-netrics/venv" ];
 then
+	echo "activating virtual environment" >> "test.txt"
 	. ./venv/bin/activate
 fi
 requirements="$VCA_PATH/requirements.txt"
-python3 -m pip install -r $requirements
+pip3 install -r $requirements
+
+## switch to a new user
+## download the vca code
+git submodule update --init --recursive
+git submodule update --remote
+
 
