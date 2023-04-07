705bb27c491ee8e9dfc4bb7729de64e173a4fd2d
tarunmangla <mangla.tarun92@gmail.com>
2023-04-07 15:41:19 +0000

    vca installation scripts

--- scripts/make_vca.sh
+++ scripts/make_vca.sh
@@ -8,7 +8,9 @@ git submodule update --remote --recursive
 
 
 ## copy the config file
-VCA_PATH="src/netrics/plugin/netrics_vca_test"
-VCA_AUTO_PATH="$VCA_PATH/vca_automation"
-cp "$VCA_AUTO_PATH/configs/config_client.toml_bkp" "$VCA_AUTO_PATH/config.toml"
+vca_path="src/netrics/plugin/netrics_vca_test/vca_automation"
+cp "$vca_path/configs/config_client.toml_bkp" "$vca_path/config.toml"
+config_file="$vca_path/config.toml"
+download_dir="/home/netrics/snap/chromium/current/Downloads"
+sed -i "s@download_dir=.*@download_dir='$download_dir'@" $config_file
 
