5d4f24ae5cb862d8ff0b9e25746da5368721aea2
Fathima Maha <fathima.nitt@gmail.com>
2023-04-13 20:25:11 +0000

    plugins: fix make install

--- src/netrics/plugins/plugin_httping.py
+++ src/netrics/plugins/plugin_httping.py
@@ -8,20 +8,27 @@ log = logging.getLogger(__name__)
 def test_httping(key, conf, results, quiet=False):
 
     #read the sites from conf
-    #TODO
-    sites = [("www.wikipedia.com","wikipedia"),("www.google.com","google")]
+    try:
+        labels = conf['reference_site_dict']
+        sites = conf['httping']['targets']
+    except Exception as err:
+        print("config file not configured correctly for httping: ",err)
+        results[key][f'error'] = f'{err}'
+        log.error(err)
+        return
 
     #take argument how many times to run from conf
-    #TODO
-    c = 2
+    c = conf['httping']['c']
 
     ping_res = {}
 
-    for site,label in sites:
+    for site in sites:
+
+        label = labels[site]
         
         ping_cmd = f"httping {site} -c {c}"
         ping_res[label], err = popen_exec(ping_cmd)
-
+        
         if len(err) > 0:            
             print(f"ERROR: {err}")
             results[key][label][f'error'] = f'{err}'
@@ -50,9 +57,9 @@ def test_httping(key, conf, results, quiet=False):
             print(f'Average value: {avg_value} (ms)')
             print(f'Maximum value: {max_value} (ms)')
 
-        results[key][f'{label}_min_value'] = min_value
-        results[key][f'{label}_avg_value'] = avg_value
-        results[key][f'{label}_max_value'] = max_value
+        results[key][f'{label}_httping_min_value'] = min_value
+        results[key][f'{label}_httping_avg_value'] = avg_value
+        results[key][f'{label}_httping_max_value'] = max_value
 
     return ping_res
 
