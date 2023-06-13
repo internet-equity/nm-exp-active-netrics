4ff7145c531336224747c755e0e8cf61cdd8d741
Ranya Sharma <ranyasharma2004@gmail.com>
2023-06-13 21:13:17 +0000

    fix: missing valid ip

--- src/netrics/utils.py
+++ src/netrics/utils.py
@@ -6,3 +6,16 @@ def popen_exec(cmd):
     err = pipe.stderr.read().decode('utf-8')
     return out, err
 
+# function taken from
+# https://stackoverflow.com/questions/11264005/using-a-regex-to-match-ip-addresses
+def valid_ip(address):
+    """
+    Check if a site in self.sites is an IP address
+    """
+    try:
+        host_bytes = address.split('.')
+        valid = [int(b) for b in host_bytes]
+        valid = [b for b in valid if b >=0 and b<=255]
+        return len(host_bytes) == 4 and len(valid) == 4
+    except:
+        return False
