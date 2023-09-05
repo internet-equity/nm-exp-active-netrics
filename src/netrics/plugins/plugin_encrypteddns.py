7cbe1a73a88a298e995d4ab9c733e44e738ad27d
Ranya Sharma <ranyasharma2004@gmail.com>
2023-09-05 18:32:27 +0000

    ping functionality

--- src/netrics/plugins/plugin_encrypteddns.py
+++ src/netrics/plugins/plugin_encrypteddns.py
@@ -35,7 +35,9 @@ def test_encrypteddns(key, conf, results, quiet = False):
             print(f'RUNNING: {resolver} {site}')
             dig_cmd = f'timeout 5 /usr/local/src/nm-exp-active-netrics/bin/dig.sh +https @{resolver} {site}'
             print(dig_cmd)
+            ping_cmd = f'ping -c 5 {resolver}'
             dig_res_pipe[f'{resolver}_{label}'] = popen_exec_pipe(dig_cmd)
+            dig_res_pipe[f'ping_{resolver}_{label}'] = popen_exec_pip(ping_cmd)
     for site in enc_sites:
          if valid_ip(site):
              continue
@@ -45,7 +47,14 @@ def test_encrypteddns(key, conf, results, quiet = False):
              label = site
          for resolver in resolvers:
              out = dig_res_pipe[f'{resolver}_{label}'].stdout.read().decode('utf-8')
+             ping_out = dig_res_pipe[f'ping_{resolver}_{label}'].stdout.read().decode('utf-8')
              err = dig_res_pipe[f'{resolver}_{label}'].stderr.read().decode('utf-8')
+             ping_err = dig_res_pipe[f'ping_{resolver}_{label}'].stderr.read().decode('utf-8')
+             if len(ping_err) > 0:
+                 print(f"PING ERROR: {ping_err}")
+                 self.results[key][f'ping_{resolver}_{label}'] = f'{ping_err}'
+                 dig_res[f'ping_{resolver}_{label}'] = { 'error': f'{ping_err}' }
+                 continue
              if len(err) > 0:
                  print(f"ERROR: {err}")
                  results[key][f'{resolver}_{label}_error'] = f'{err}'
@@ -54,9 +63,12 @@ def test_encrypteddns(key, conf, results, quiet = False):
                  error_found = True
                  continue
              dig_res[f'{resolver}_{label}'] = out
+             dig_res[f'ping_{resolver}_{label}'] = ping_out
              try:
                 dig_res_qt = re.findall('Query time: ([0-9]*) msec',dig_res[f'{resolver}_{label}'], re.MULTILINE)[0]
+                ping_time = re.findall('rtt min/avg/max/mdev = ([0-9]*)/([0-9]*)/([0-9]*)/([0-9]*) ms', ping[f'ping_{resolver}_{label}'], re.MULTILINE)[0]
                 results[key][f'{resolver}_{label}_encrypted_dns_latency'] = int(dig_res_qt)
+                results[key][f'ping_{resolver}_{label}_encrypted_dns_latency'] = int(ping_time)
              except IndexError as e:
                  print(f"ERROR: encrypted DNS lookup failed for {resolver} {site}")
                  continue
